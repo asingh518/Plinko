3a72ffc6d1ac1aa97a36a8e0705cca2917c28548
asingh518 <71722439+asingh518@users.noreply.github.com>
2021-02-28 10:09:13 -0500

    Add files via upload

---
+++ plinko.js
@@ -0,0 +1,23 @@
+class Plinko
+{
+    constructor(x,y)
+    {
+        var options=
+        {
+            isStatic:true,
+            
+            
+        }
+        this.body=Bodies.circle(x,y,10,options);
+        World.add(world,this.body);
+
+    }
+
+    display()
+    {
+        var pos=this.body.position;
+        
+        fill("white");
+        
+    }
+}
